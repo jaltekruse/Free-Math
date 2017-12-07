72b92c26e899520a104dbc386b30c8fff80f45e6
Jason Altekruse <altekrusejason@gmail.com>
2017-12-07 08:40:15 -0800

    Entire freemath app imported

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { rootReducer } from './App';
+import { render } from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 window.onload = function() {
+	const Redux = window.Redux;
     var MathQuill = window.MathQuill;
-    MathQuill = MathQuill.getInterface(1);
-    ReactDOM.render(<App />, document.getElementById('root'));
+    window.MathQuill = MathQuill.getInterface(1);
+    window.store = Redux.createStore(rootReducer);
+    window.store.subscribe(render);
+	render();
 };
 registerServiceWorker();
