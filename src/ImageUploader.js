5024b2f164c50c5a6b6def40e9d50679837741e0
Jason Altekruse <altekrusejason@gmail.com>
2019-04-01 14:22:08 -0500

    Can display a user selected image on canvas

--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MathInput from './MathInput.js';
+import ReactDOM from 'react-dom';
 
 var ImageUploader = React.createClass({
 	getInitialState () {
@@ -7,46 +8,10 @@ var ImageUploader = React.createClass({
 	},
 	render : function() {
 
-		var MAX_HEIGHT = 100;
-		var render = function(src){
-			var img = new Image();
-			img.onload = function(){
-				if(img.height > MAX_HEIGHT) {
-					img.width *= MAX_HEIGHT / img.height;
-					img.height = MAX_HEIGHT;
-				}
-				var ctx = this.refs.canvas.getContext("2d");
-				ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
-				this.refs.preview.style.width = img.width + "px";
-				this.refs.preview.style.height = img.height + "px";
-				this.refs.canvas.width = img.width;
-				this.refs.canvas.height = img.height;
-				ctx.drawImage(img, 0, 0, img.width, img.height);
-			};
-			img.src = src;
-		};
-
-		var readImage = function(imgFile){
-			if(!imgFile.type.match(/image.*/)){
-				alert("The dropped file is not an image: " + imgFile.type);
-				return;
-			}
-
-			var reader = new FileReader();
-			reader.onload = function(e){
-				render(e.target.result);
-			};
-			reader.readAsDataURL(imgFile);
-		};
 		return (<div>
 			<br />
 			<br />
-			<div id="dropTarget" ondrop={function(e){
-				alert("preventing default");
-				e.preventDefault(); 
-				readImage(e.dataTransfer.files[0]);
-			}} dragover={function(e) {e.preventDefault();}}>
-			Drop image here</div>
+                        <input type="file" ref="chooseFile" id="open-file-input" />
 			<div ref="preview">
 			</div>
 			<canvas ref="canvas"></canvas>
@@ -54,22 +19,42 @@ var ImageUploader = React.createClass({
 	},
 
     	componentDidMount: function() {
+            var MAX_HEIGHT = 300;
+            var render = function(src){
+                    var img = new Image();
+                    img.onload = function(){
+                            if(img.height > MAX_HEIGHT) {
+                                    img.width *= MAX_HEIGHT / img.height;
+                                    img.height = MAX_HEIGHT;
+                            }
+                            var imgDiv = ReactDOM.findDOMNode(this.refs.canvas);
+                            var ctx = this.refs.canvas.getContext("2d");
+                            ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
+                            this.refs.preview.style.width = img.width + "px";
+                            this.refs.preview.style.height = img.height + "px";
+                            this.refs.canvas.width = img.width;
+                            this.refs.canvas.height = img.height;
+                            ctx.drawImage(img, 0, 0, img.width, img.height);
+                    }.bind(this);
+                    img.src = src;
+            }.bind(this);
+
+            var chooseFile = ReactDOM.findDOMNode(this.refs.chooseFile);
+            chooseFile.addEventListener("change", 
+                function(evt) {
+                    var imgFile = evt.target.files[0];
+                    if(!imgFile.type.match(/image.*/)){
+                            alert("The dropped file is not an image: " + imgFile.type);
+                            return;
+                    }
+
+                    var reader = new FileReader();
+                    reader.onload = function(e){
+                            render(e.target.result);
+                    };
+                    reader.readAsDataURL(imgFile);
+                });
 	}
-	/*
-	 	require(["dojo/dom", "dojo/domReady!"], function(dom){
-		var target = document.getElementById("drop-target"),
-		var preview = document.getElementById("preview"),
-		var canvas = document.getElementById("canvas");
-
-
-		//	DOMReady setup
-		target.addEventListener("dragover", function(e) {e.preventDefault();}, true);
-		target.addEventListener("drop", function(e){
-			e.preventDefault(); 
-			readImage(e.dataTransfer.files[0]);
-		}, true);
-	 */
-
 });
 
 export default ImageUploader; 
