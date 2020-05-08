9374c1f4d5ddb78d3d2739cbe00ac286b92161c2
Jason Altekruse <altekrusejason@gmail.com>
2020-05-08 09:19:06 -0500

    WIP

--- src/DefaultHomepageActions.js
+++ src/DefaultHomepageActions.js
@@ -210,7 +210,14 @@ class UserActions extends React.Component {
         var openDriveAssignments = function(assignment) {
             window.loadStudentDocsFromZip = loadStudentDocsFromZip;
             window.downloadClassAssignmentFiles(
-                assignment.assignment.studentWorkFolder.id, function(response) {});
+                assignment.assignment.studentWorkFolder.id, function(response) {
+                    console.log(response);
+                    window.downloadFile(response, true, function(data) {
+                        setTimeout(function() {
+                            loadStudentDocsFromZip(data, "temp", response.res);
+                        }.bind(this), 50);
+                    });
+                });
         }
 
         var recoverAutoSaveCallback = function(autoSaveFullName, filename, appMode) {
