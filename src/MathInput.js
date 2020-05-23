02e712e108a970f2370fe4bc1a4261b0e99bea89
Jason Altekruse <altekrusejason@gmail.com>
2020-05-23 10:56:58 -0500

    Fix hover text, remove extra backslash

--- src/MathInput.js
+++ src/MathInput.js
@@ -286,7 +286,7 @@ class TexButtons extends React.Component {
                         }}>
                         <TeX>{"\\downarrow"}</TeX>
                     </button>
-                    <button title="\\text [Enter]" className="tex-button wide-tex-button"
+                    <button title={"\\text [Enter]"} className="tex-button wide-tex-button"
                         style={{display: "inline-block", float: "none"}}
                               onClick={() => {
                             this.props.onInsert(input => {
