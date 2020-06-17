759b38503d6fa3706a5095393d15d43584317639
Jason Altekruse <altekrusejason@gmail.com>
2020-06-17 08:44:05 -0500

    WIP - render the problem editor as a root component
    - with the reducer for the component set as the root store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import './index.css';
 import { rootReducer, ephemeralStateReducer } from './FreeMath';
-import { render } from './DefaultHomepageActions';
-import { autoSave } from './FreeMath.js';
+import Problem, { problemReducer } from './Problem.js';
+//import { render } from './DefaultHomepageActions';
+import { autoSave, getCompositeState} from './FreeMath.js';
 import { unregister } from './registerServiceWorker';
 
+var MathQuill = window.MathQuill;
+
+var BUTTON_GROUP = 'BUTTON_GROUP';
+
+function render() {
+    window.MathQuill = MathQuill.getInterface(1);
+    const globalState = getCompositeState();
+    ReactDOM.render(
+        <div>
+            <Problem buttonGroup={globalState[BUTTON_GROUP]} value={globalState} />
+        </div>,
+        document.getElementById('root')
+    );
+}
+
 window.onload = function() {
+    window.store = createStore(problemReducer);
+    window.store.subscribe(render);
+    window.ephemeralStore = createStore(ephemeralStateReducer);
+    window.ephemeralStore.subscribe(render);
+    render();
+}
+
+
+window.DISABLED_onload = function() {
     /* No longer necessary, figured out how to set up server level https
     var location = window.location;
     if (location.hostname !== "localhost" && location.protocol !== 'https:')
